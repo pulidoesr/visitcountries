b9516e6621b0b89c7ab144335ac315ebaed16838
pulidoesr <pul21010@byui.edu>
2024-10-22 08:23:22 -0500

    w06 correction of utils folders

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -9,9 +9,9 @@ async function loadTemplate(path) {
 // General loader Header & Footer
 
 export async function loadHeaderFooter() {
-  const headerTemplate = await loadTemplate("/src/public/partials/header.html");
+  const headerTemplate = await loadTemplate("./src/public/partials/header.html");
   const headerElement = document.querySelector("#main-header");
-  const footerTemplate = await loadTemplate("/src/public/partials/footer.html");
+  const footerTemplate = await loadTemplate("./src/public/partials/footer.html");
   const footerElement = document.querySelector("#main-footer");
   renderWithTemplate(headerTemplate, headerElement);
   renderWithTemplate(footerTemplate, footerElement);
