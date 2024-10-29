434f3c5d4189956db03991bf0648bb223f6c9628
pulidoesr <pul21010@byui.edu>
2024-10-28 20:55:08 -0500

    Removing the console verification messages

--- src/js/main.js
+++ src/js/main.js
@@ -4,15 +4,7 @@ import { loadHeaderFooter } from './utils.mjs';
  // load Header and Footer
  loadHeaderFooter();
  
-/* function goToPage(option) {
-    if (option === 'name') {
-        window.location.href = './src/pages/countriesbyname.html';
-    } else if (option === 'region') {
-        window.location.href = './src/pages/countriesbyregion.html';
-    } else if (option === 'language') {
-        window.location.href = 'review-by-language.html';
-    }
-} */
+
 
 
 
